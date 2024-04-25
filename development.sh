815beb008dabfbf362b5863ce601b34e1d2435c9
ananthvk <123456789987654321sr@gmail.com>
2024-04-26 00:04:45 +0530

    made static library as default

--- development.sh
+++ development.sh
@@ -2,6 +2,6 @@
 # For development, set compiler to clang
 mkdir -p subprojects
 meson wrap install gtest
-CXX=clang++ CC=clang meson setup -Db_sanitize=address -Db_lundef=false --reconfigure builddir
+CXX=clang++ CC=clang meson setup -Db_sanitize=address -Db_lundef=true -Ddefault_library=static --reconfigure builddir
 cd builddir
 meson test

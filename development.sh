f6f4ccbf2a539e1d740c0c568fc2724fc5fed421
ananthvk <123456789987654321sr@gmail.com>
2024-04-26 21:56:36 +0530

    Modified build file

--- development.sh
+++ development.sh
@@ -2,6 +2,6 @@
 # For development, set compiler to clang
 mkdir -p subprojects
 meson wrap install gtest
-CXX=clang++ CC=clang meson setup -Db_sanitize=address -Db_lundef=true -Ddefault_library=static --reconfigure builddir
+CXX=clang++ CC=clang meson setup -Db_sanitize=address -Db_lundef=false -Ddefault_library=shared --reconfigure builddir
 cd builddir
 meson test

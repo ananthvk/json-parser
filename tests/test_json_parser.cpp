859f045c287757a9382ae1f9dbe7f8f7f35b68ea
ananthvk <123456789987654321sr@gmail.com>
2024-04-26 17:27:29 +0530

    Added a few more tests

--- tests/test_json_parser.cpp
+++ tests/test_json_parser.cpp
@@ -62,6 +62,64 @@ TEST(JSONParser, SimpleValues)
     ASSERT_EQ(tree["key"].type, JSONObjectType::NULL_VALUE);
 }
 
+TEST(JSONParser, KeyValuePairs)
+{
+    JSONParser parser;
+    parser.parse(
+        R"( {"a": "string value", "b": 3.1415, "c": true, "d": false, "e": null, "f": 881454} )");
+    auto tree = parser.get_tree();
+
+    ASSERT_EQ(tree.type, JSONObjectType::OBJECT);
+    ASSERT_EQ(tree.size(), 6);
+
+    ASSERT_EQ(tree["a"].as_string(), "string value");
+    ASSERT_NEAR(tree["b"].as_real(), 3.1415, 1e-5);
+    ASSERT_EQ(tree["c"].as_bool(), true);
+    ASSERT_EQ(tree["d"].as_bool(), false);
+    ASSERT_EQ(tree["e"].type, JSONObjectType::NULL_VALUE);
+    ASSERT_EQ(tree["f"].as_integer(), 881454);
+}
+
+TEST(JSONParser, NestedObjects)
+{
+    JSONParser parser;
+    parser.parse(R"( {"a": {"b":{ "c": { "d": { "key": "this is a value" } } } } } )");
+    auto tree = parser.get_tree();
+    ASSERT_EQ(tree["a"]["b"]["c"]["d"]["key"].as_string(), "this is a value");
+}
+
+TEST(JSONParser, Arrays)
+{
+    JSONParser parser;
+    parser.parse(R"( [1, 2, 3, 4, 5] )");
+    auto tree = parser.get_tree();
+    ASSERT_EQ(tree.type, JSONObjectType::ARRAY);
+    ASSERT_EQ(tree.size(), 5);
+    auto v = tree.as_vector();
+    for (int i = 1; i <= 5; i++)
+    {
+        ASSERT_EQ(v[i - 1].as_integer(), i);
+    }
+}
+
+TEST(JSONParser, MixedArrays)
+{
+    JSONParser parser;
+    parser.parse(R"( [1, 2.314, true, false, null, {}, [], "this is"] )");
+    auto tree = parser.get_tree();
+    ASSERT_EQ(tree.type, JSONObjectType::ARRAY);
+    ASSERT_EQ(tree.size(), 8);
+    auto v = tree.as_vector();
+    ASSERT_EQ(v[0].type, JSONObjectType::NUMBER_INT);
+    ASSERT_EQ(v[1].type, JSONObjectType::NUMBER_REAL);
+    ASSERT_EQ(v[2].type, JSONObjectType::BOOLEAN);
+    ASSERT_EQ(v[3].type, JSONObjectType::BOOLEAN);
+    ASSERT_EQ(v[4].type, JSONObjectType::NULL_VALUE);
+    ASSERT_EQ(v[5].type, JSONObjectType::OBJECT);
+    ASSERT_EQ(v[6].type, JSONObjectType::ARRAY);
+    ASSERT_EQ(v[7].type, JSONObjectType::STRING);
+}
+
 int main(int argc, char *argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);
